7c72b17a9bf99916998b5b020b8bf60f439d846e
BrendaBrigida <142326007+BrendaBrigida@users.noreply.github.com>
2025-03-06 16:43:48 -0300

    Add files via upload

---
+++ variaveisOperadores.js
@@ -0,0 +1,14 @@
+let numA = 30;
+let numB = 19;
+
+let soma = numA + numB;
+let sub = numA - numB;
+let mult = numA * numB;
+let div = numA / numB;
+
+console.log(`Resultado da soma entre ${numA} E ${numB} é ${soma}`);
+console.log(`Resultado da subtracao entre ${numA} E ${numB} é ${sub}`);
+console.log(`Resultado da multiplicaçao entre ${numA} E ${numB} é ${mult}`);
+console.log(`Resultado da divisão entre ${numA} E ${numB} é ${div}`);
+
+

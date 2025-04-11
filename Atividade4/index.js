493011592d5e506d584fce26ca46f32fd35ecff1
BrendaBrigida <brendabrigidamendes@gmail.com>
2025-04-11 09:48:31 -0300

    app_web

--- Atividade4/index.js
+++ Atividade4/index.js
@@ -1,39 +1,39 @@
 const express = require('express');
-const estoque = require(`./estoque`);
-const app = express();
+const estoque = require('./estoque');
+const app     = express();
 
 app.get('/', (req, res)=>{
-    let html = '<h1>app_estoque</h1>';
-    html     += '<h3>Rotas disponiveis:</h3>'
-    html     += '<p>/adicionar/:id/:nome/:qtd</p>'
-    html     += '<p>/remover/:id</p>'
-    html     += '<p>/editar/:id/:qtd</p>'
+    let html =  '<h1>app_estoque</h1>';
+    html     += '<h3>Rotas disponíveis:</h3>';
+    html     += '<p>/adicionar/:id/:nome/:qtd</p>';
+    html     += '<p>/listar</p>';
+    html     += '<p>/remover/:id</p>';
+    html     += '<p>/editar/:id/:qtd</p>';
     res.send(html);
 });
 
-app.get('/adicionar/:id/:nome/:qtd', (req,res)=>{
+app.get('/adicionar/:id/:nome/:qtd', (req, res)=>{
     let item = {
-        id: Number(req.params.id),
+        id  : Number(req.params.id),
         nome: req.params.nome,
-        qtd: Number(req.params.qtd)
+        qtd : Number(req.params.qtd)
     };
-
-    res.send(item); /*devolve pro usuaria*/
+    res.send(estoque.adicionar(item));
 });
 
-app.get('/listar', (req,res)=>{
+app.get('/listar', (req, res)=>{
     res.send(estoque.listar());
 });
 
-app.get('/remover/:id', (req,res)=>{
+// /remover/:id
 
+app.get('/editar/:id/:qtd', (req, res)=>{
+    let id  = Number(req.params.id);
+    let qtd = Number(req.params.qtd);
+    res.send(estoque.editar(id, qtd));
 });
 
-app.get('/editar/:id/:qtd', (req,res)=>{
-
-});
-
-const PORT  = 8080;
+const PORT = 8080;
 app.listen(PORT, ()=>{
     console.log('app rodando na porta ' + PORT);
 });

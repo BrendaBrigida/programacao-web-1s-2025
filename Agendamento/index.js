a85d305efbd27101c211a745da4cae50be4566cf
BrendaBrigida <brendabrigidamendes@gmail.com>
2025-06-06 09:45:00 -0300

    AgendamentocomBD

--- Agendamento/index.js
+++ Agendamento/index.js
@@ -1,10 +1,27 @@
-const express = require('express');
+const express = require('express'); 
 const mustacheExpress = require('mustache-express');
+const db = require('./db');
 const app = express();
 
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
+app.use(express.urlencoded({extended: true}));
+
+const agendamentoRouter = require('./routers/agendamentoRouter');
+app.use('/', agendamentoRouter);
+
+db.sync();
+
+//data
+const data = new Date()
+
+const dia = String(data.getDate()).padStart(2, '0') //1 -> 01
+const mes = String(data.getMonth() + 1).padStart(2, '0')
+const ano = data.getFullYear()
+
+const dataAtual = `${dia}/${mes}/${ano}`
+
 
 const PORT = 8080;
 app.listen(PORT, ()=>{
